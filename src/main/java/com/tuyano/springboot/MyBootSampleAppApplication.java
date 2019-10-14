e883634c7bb364116d8086d14c8ff7614ac4aefb
広記 由井 <173224@ocsjoho.onmicrosoft.com>
2019-10-14 13:29:25 +0900

    First Commit

---
+++ src/main/java/com/tuyano/springboot/MyBootSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.tuyano.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootSampleAppApplication.class, args);
+	}
+
+}
